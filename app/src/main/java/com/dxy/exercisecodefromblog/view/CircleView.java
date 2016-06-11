d0a144c88269d0ac19a0d82e4dbb1d3fb4d56585
dangxueyi <dangxy99@163.com>
2016-06-11 23:07:49 +0800

    1.view之scale

--- app/src/main/java/com/dxy/exercisecodefromblog/view/CircleView.java
+++ app/src/main/java/com/dxy/exercisecodefromblog/view/CircleView.java
@@ -4,9 +4,9 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
-import android.graphics.RectF;
 import android.util.AttributeSet;
 import android.view.View;
+import android.view.WindowManager;
 
 /**
  * Created by author_dang on 16/6/10.
@@ -19,6 +19,8 @@ public class CircleView extends View {
 
     private float mWidth;
 
+    private Context mContext;
+
     public CircleView(Context context) {
         this(context, null);
     }
@@ -29,7 +31,7 @@ public class CircleView extends View {
 
     public CircleView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
-
+        mContext= context;
         initData();
     }
 
@@ -48,47 +50,70 @@ public class CircleView extends View {
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
 
-        mHeigth = getWidth();
+        WindowManager windowManager =(WindowManager) mContext.getSystemService(Context.WINDOW_SERVICE);
+        mHeigth = windowManager.getDefaultDisplay().getHeight();
 
-        mWidth = getHeight();
+        mWidth =windowManager.getDefaultDisplay().getWidth();
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
 
 
-        mPaint.setColor(Color.BLACK);
+//        mPaint.setColor(Color.BLACK);
+//
+//        canvas.translate(mWidth/2,mHeigth/2);
+//
+//        canvas.drawCircle(0, 0, 100, mPaint);
+//
+//
+//        mPaint.setColor(Color.BLACK);
+//
+//        canvas.translate(200, 200);
+//
+//        canvas.drawCircle(0, 0, 100, mPaint);
+//
+//
+//
+//
+//        RectF  rectF = new RectF(0,-400,400,0);
+//
+//        mPaint.setStyle(Paint.Style.STROKE);
+//
+//        mPaint.setColor(Color.BLACK);
+//
+//        canvas.drawRect(rectF,mPaint);
+//
+//
+//        canvas.scale(0.5f,0.5f);
+//
+//
+//
+//        mPaint.setColor(Color.BLUE);
+//
+//        canvas.drawRect(rectF,mPaint);
 
-        canvas.translate(200, 200);
-
-        canvas.drawCircle(0, 0, 100, mPaint);
-
-
-        mPaint.setColor(Color.BLACK);
-
-        canvas.translate(200, 200);
-
-        canvas.drawCircle(0, 0, 100, mPaint);
-
-        canvas.translate(200,200);
-
-
-        RectF  rectF = new RectF(0,-400,400,0);
-
-        mPaint.setStyle(Paint.Style.STROKE);
-
-        mPaint.setColor(Color.BLACK);
-
-        canvas.drawRect(rectF,mPaint);
-
-
-        canvas.scale(0.5f,0.5f);
-
-
+        canvas.translate(mWidth/2,mHeigth/2);
 
         mPaint.setColor(Color.BLUE);
 
-        canvas.drawRect(rectF,mPaint);
+        mPaint.setStrokeWidth(20);
+
+        mPaint.setStyle(Paint.Style.STROKE);
+
+
+
+
+        for(int i = 0 ;i<=20;i++ ){
+            canvas.scale(0.9f,0.9f);
+
+            canvas.drawCircle(0,0,mWidth/2,mPaint);
+
+        }
+
+
+
+
 
     }
 }
