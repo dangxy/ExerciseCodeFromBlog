10fafff1fb2e200405f106710b3eab62ce6528c5
dangxueyi <dangxy99@163.com>
2016-06-13 22:01:47 +0800

    1.图库获取图片

--- app/src/main/java/com/dxy/exercisecodefromblog/takephoto/PhotoActivity.java
+++ app/src/main/java/com/dxy/exercisecodefromblog/takephoto/PhotoActivity.java
@@ -9,6 +9,7 @@ import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 import com.dxy.exercisecodefromblog.R;
 
@@ -19,12 +20,14 @@ import java.util.Date;
 import butterknife.ButterKnife;
 import butterknife.InjectView;
 
-public class PhotoActivity extends AppCompatActivity implements View.OnClickListener{
+public class PhotoActivity extends AppCompatActivity implements View.OnClickListener {
 
     @InjectView(R.id.take_photo_camera)
     Button takePhotoCamera;
     @InjectView(R.id.take_photo_tu_ku)
     Button takePhotoTuKu;
+    @InjectView(R.id.iv_photo_view)
+    ImageView ivPhotoView;
 
     private Uri imageUrl;
 
@@ -37,7 +40,7 @@ public class PhotoActivity extends AppCompatActivity implements View.OnClickList
         initData();
     }
 
-    private void initData(){
+    private void initData() {
         takePhotoCamera.setOnClickListener(this);
 
         takePhotoTuKu.setOnClickListener(this);
@@ -45,36 +48,41 @@ public class PhotoActivity extends AppCompatActivity implements View.OnClickList
 
     @Override
     public void onClick(View v) {
-        switch (v.getId()){
+        switch (v.getId()) {
             case R.id.take_photo_camera:
 
-              if(Environment.MEDIA_MOUNTED.equals(Environment.getExternalStorageState())){
-                  File sdcardDir = Environment.getExternalStorageDirectory();
-                  String path = sdcardDir.getPath()+"/cardImages";
+                if (Environment.MEDIA_MOUNTED.equals(Environment.getExternalStorageState())) {
+                    File sdcardDir = Environment.getExternalStorageDirectory();
+                    String path = sdcardDir.getPath() + "/cardImages";
 
-                  File photofile = new File(path);
+                    File photofile = new File(path);
 
-                  if(!photofile.exists()){
-                      photofile.mkdirs();
-                  }else{
-                      imageUrl =   Uri.fromFile(new File(photofile,getHeadPictureName()));
+                    if (!photofile.exists()) {
+                        photofile.mkdirs();
+                    } else {
+                        imageUrl = Uri.fromFile(new File(photofile, getHeadPictureName()));
 
-                      Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                      intent.setAction(MediaStore.ACTION_IMAGE_CAPTURE);
-                      intent.putExtra(MediaStore.EXTRA_OUTPUT,imageUrl);
-                      startActivityForResult(intent,1);
-                  }
+                        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                        intent.setAction(MediaStore.ACTION_IMAGE_CAPTURE);
+                        intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUrl);
+                        startActivityForResult(intent, 1);
+                    }
 
-              }
-
-
-
-
+                }
 
 
                 break;
 
             case R.id.take_photo_tu_ku:
+                try {
+                    Intent intent = new Intent();
+
+                    intent.setType("image/*");
+                    intent.setAction(Intent.ACTION_GET_CONTENT);
+                    startActivityForResult(intent, 2);
+                } catch (Exception e) {
+                    e.getStackTrace();
+                }
                 break;
 
         }
@@ -94,18 +102,18 @@ public class PhotoActivity extends AppCompatActivity implements View.OnClickList
 
         Uri uri = null;
 
-        if(data==null){
-            uri=imageUrl;
-        }else{
-            uri= data.getData();
+        if (data == null) {
+            uri = imageUrl;
+        } else {
+            uri = data.getData();
         }
-        if(resultCode==RESULT_OK){
+        if (resultCode == RESULT_OK) {
 
-            switch (requestCode){
+            switch (requestCode) {
                 case 1:
-                    intent.setDataAndType(uri,"image/*");
+                    intent.setDataAndType(uri, "image/*");
 
-                    intent.putExtra("crop",true);
+                    intent.putExtra("crop", true);
                     intent.putExtra("aspectX", 1);
                     intent.putExtra("aspectY", 1);
                     intent.putExtra("outputX", 320);
@@ -115,21 +123,55 @@ public class PhotoActivity extends AppCompatActivity implements View.OnClickList
                     startActivityForResult(intent, 3);
 
                     break;
+
+                case 2:
+                    intent.setDataAndType(uri, "image/*");
+                    intent.putExtra("crop", true);
+                    intent.putExtra("aspectX", 1);
+                    intent.putExtra("aspectY", 1);
+                    intent.putExtra("outputX", 320);
+                    intent.putExtra("outputY", 320);
+                    intent.putExtra("return-data", true);
+                    // 创建一个文件夹对象，赋值为外部存储器的目录
+                    File sdcardDir = Environment.getExternalStorageDirectory();
+                    // 得到一个路径，内容是sdcard的文件夹路径和名字
+                    String path = sdcardDir.getPath()
+                            + "/FollowUpHelper/cardImages";
+
+                    File photofile = new File(path);
+                    uri = Uri.fromFile(new File(photofile, getHeadPictureName()));
+                    intent.putExtra(MediaStore.EXTRA_OUTPUT, uri);
+                    startActivityForResult(intent, 3);
+
+                    break;
                 case 3:
-                    if(data!=null){
+                    if (data != null) {
                         Intent resultIntent = getIntent();
 
                         Bundle bundle = data.getExtras();
                         Bitmap myBitmap = bundle.getParcelable("data");
 
-                        resultIntent.putExtra("myBitmap",myBitmap);
+                        resultIntent.putExtra("myBitmap", myBitmap);
 
                         setResult(1,resultIntent);
 
-                        finish();
+
                     }
                     break;
+
             }
+        }else if(resultCode==1){
+            switch (requestCode) {
+                case 1:
+                    Bundle bundle = intent.getExtras();
+                    ivPhotoView.setImageBitmap((Bitmap) bundle.get("myBitmap"));
+                    break;
+
+                default:
+                    break;}
+
+
         }
+
     }
 }
