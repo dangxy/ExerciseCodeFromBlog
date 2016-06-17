190c7f4ea9797a7e286dea0ba2a280a727383af4
dangxueyi <dangxy99@163.com>
2016-06-17 23:00:11 +0800

    1.RecyclerView 设置数据

--- app/src/main/java/com/dxy/exercisecodefromblog/Recycler/RecyclerViewActivity.java
+++ app/src/main/java/com/dxy/exercisecodefromblog/Recycler/RecyclerViewActivity.java
@@ -1,15 +1,54 @@
 package com.dxy.exercisecodefromblog.Recycler;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.OrientationHelper;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.StaggeredGridLayoutManager;
 
 import com.dxy.exercisecodefromblog.R;
+import com.dxy.exercisecodefromblog.Recycler.adapter.MyRecyclerViewAdapter;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
 
 public class RecyclerViewActivity extends AppCompatActivity {
+
+    @InjectView(R.id.rv_recycler_view)
+    RecyclerView rvRecyclerView;
+
+    private Context mContext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recycler_view);
+        ButterKnife.inject(this);
+        mContext = this;
+        initData();
+    }
+
+    private void initData() {
+        int[] pic = new int[]{R.mipmap.a, R.mipmap.b, R.mipmap.c, R.mipmap.d, R.mipmap.e};
+
+        String[] title = new String[]{"美女1", "美女2", "美女3", "美女4", "美女5"};
+
+
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(mContext);
+
+        linearLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+
+        rvRecyclerView.setLayoutManager(new GridLayoutManager(mContext, 2));
+
+        rvRecyclerView.setLayoutManager(new StaggeredGridLayoutManager(2, OrientationHelper.VERTICAL));
+
+        rvRecyclerView.setLayoutManager(linearLayoutManager);
+
+        rvRecyclerView.setAdapter(new MyRecyclerViewAdapter(mContext, title, pic));
+
+
     }
 }
--- app/src/main/java/com/dxy/exercisecodefromblog/Recycler/adapter/MyRecyclerViewAdapter.java
+++ app/src/main/java/com/dxy/exercisecodefromblog/Recycler/adapter/MyRecyclerViewAdapter.java
@@ -1,15 +1,21 @@
 package com.dxy.exercisecodefromblog.Recycler.adapter;
 
 import android.content.Context;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.dxy.exercisecodefromblog.R;
 
 /**
  * Created by author_dang on 16/6/17.
  */
-public class MyRecyclerViewAdapter extends RecyclerView.Adapter<MyRecyclerViewAdapter.myRecyclerViewAdapter> {
+public class MyRecyclerViewAdapter extends RecyclerView.Adapter<MyRecyclerViewAdapter.myRecyclerViewAdapterViewHolder> {
     private LayoutInflater mLayoutInflater;
 
     private Context mContext;
@@ -29,25 +35,46 @@ public class MyRecyclerViewAdapter extends RecyclerView.Adapter<MyRecyclerViewAd
     }
 
     @Override
-    public myRecyclerViewAdapter onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+    public myRecyclerViewAdapterViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return new myRecyclerViewAdapterViewHolder(mLayoutInflater.inflate(R.layout.item_recycler_view_layout, parent, false));
     }
 
     @Override
-    public void onBindViewHolder(myRecyclerViewAdapter holder, int position) {
+    public void onBindViewHolder(myRecyclerViewAdapterViewHolder holder, final int position) {
+
+        holder.imageView.setImageResource(mPictures[position]);
+
+        holder.textView.setText(mTitles[position]);
+
+        holder.cardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(mContext, position + "", Toast.LENGTH_SHORT).show();
+            }
+        });
+
 
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return mTitles == null ? 0 : mTitles.length;
     }
 
-    public static class myRecyclerViewAdapter extends RecyclerView.ViewHolder {
+    public static class myRecyclerViewAdapterViewHolder extends RecyclerView.ViewHolder {
 
+        private CardView cardView;
 
-        public myRecyclerViewAdapter(View itemView) {
+        private ImageView imageView;
+
+        private TextView textView;
+
+        public myRecyclerViewAdapterViewHolder(View itemView) {
             super(itemView);
+
+            cardView = (CardView) itemView.findViewById(R.id.cv_item);
+            textView = (TextView) itemView.findViewById(R.id.tv_text);
+            imageView = (ImageView) itemView.findViewById(R.id.iv_pic);
 
 
         }
